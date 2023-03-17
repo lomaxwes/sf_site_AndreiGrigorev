1d8bcaf4b6d5ae0f53194228be6023422a119c92
lomaxwes <lomaxwes@gmail.com>
2023-03-17 20:09:26 +0500

    urls

--- Project/NewsPaper/NewsPaper/urls.py
+++ Project/NewsPaper/NewsPaper/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('pages/', include('django.contrib.flatpages.urls')),
     path('posts/', include('posts.urls')),
     path('accounts/', include('allauth.urls')),
+    path('account/', include('accounts.urls')),
 ]
