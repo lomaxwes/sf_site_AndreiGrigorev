bd5d285685920b56293a171e22a0e22861a7bdd0
lomaxwes <lomaxwes@gmail.com>
2023-03-17 14:07:28 +0500

    refactoring

--- Project/NewsPaper/accounts/urls.py
+++ Project/NewsPaper/accounts/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import BaseRegisterView, upgrade_me
+from .views import upgrade_me
+
+app_name = 'account'
 
 urlpatterns = [
-    # path('login/',
-    #      LoginView.as_view(template_name='accounts/login.html'),
-    #      name='login'),
-    # path('logout/',
-    #      LogoutView.as_view(template_name='accounts/logout.html'),
-    #      name='logout'),
-    # path('signup/',
-    #      BaseRegisterView.as_view(template_name='accounts/signup.html'),
-    #      name='signup'),
     path('upgrade/', upgrade_me, name='upgrade'),
 ]
--- Project/NewsPaper/accounts/views.py
+++ Project/NewsPaper/accounts/views.py
@@ -1,15 +1,9 @@
 from django.contrib.auth.models import User, Group
 from django.views.generic.edit import CreateView
-from .models import BaseRegisterForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from ..posts.models import Author
 
-
-# class BaseRegisterView(CreateView):
-#     model = User
-#     form_class = BaseRegisterForm
-#     success_url = '/'
 
 
 @login_required
