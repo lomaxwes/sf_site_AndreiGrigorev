198af4773490217eeaa527d5926bd9396ed28b79
lomaxwes <lomaxwes@gmail.com>
2023-03-31 11:44:02 +0500

    Weekly notifications

--- Project/NewsPaper/posts/signals.py
+++ Project/NewsPaper/posts/signals.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import m2m_changed
@@ -5,7 +7,7 @@ from django.dispatch import receiver
 from django.template.loader import render_to_string
 
 from NewsPaper.settings import SITE_URL
-from .models import PostCategory
+from .models import PostCategory, Category, Post
 
 
 def send_notifications(preview, pk, title, subscribers):
@@ -26,6 +28,31 @@ def send_notifications(preview, pk, title, subscribers):
 
     msg.attach_alternative(html_context, 'text/html')
     msg.send()
+
+
+def send_weekly_notifications():
+
+    categories = Category.objects.all()
+    for category in categories:
+        subscribers = category.subscribers.all()
+        posts = Post.objects.filter(category=category, time_date__gte=datetime.now() - datetime.timedelta(days=7))
+        if posts.exists():
+            subject = f'Новые статьи в категории {category.get_category_name_display()}'
+            html_context = render_to_string(
+                'weekly_notifications_email.html',
+                {
+                    'category': category,
+                    'posts': posts,
+                }
+            )
+            msg = EmailMultiAlternatives(
+                subject=subject,
+                body='',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                to=[s.email for s in subscribers],
+            )
+            msg.attach_alternative(html_context, 'text/html')
+            msg.send()
 
 
 @receiver(m2m_changed, sender=PostCategory)
