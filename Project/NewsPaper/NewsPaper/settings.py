be30af028968bca990d9dad742a241817e1f95f1
lomaxwes <lomaxwes@gmail.com>
2023-03-31 15:11:57 +0500

    email confirmation and greeting

--- Project/NewsPaper/NewsPaper/settings.py
+++ Project/NewsPaper/NewsPaper/settings.py
@@ -72,7 +72,7 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_FORMS = {'signup': 'accounts.forms.CommonSignupForm'}
 
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
