4fbbc4da3f24a4578340e1d3aaf667649494a1f2
lomaxwes <lomaxwes@gmail.com>
2023-04-01 10:48:24 +0500

    update

---
+++ Project/NewsPaper/NewsPaper/secret_settings.py
@@ -0,0 +1,5 @@
+
+DJN_KEY = 'django-insecure-6ttv8sm*g&e@5-61(x0=t!nlglih3t3o%*4=@_9i_rqk#(f251'
+YA_EML = 'NAS35-25a+13m'
+MY_EML = 'aidigo.grigorjev@yandex.ru'
+YA_NAME = 'aidigo.grigorjev'
--- Project/NewsPaper/posts/signals.py
+++ Project/NewsPaper/posts/signals.py
@@ -31,30 +31,6 @@ def send_notifications(preview, pk, title, subscribers):
     msg.send()
 
 
-def send_weekly_notifications():
-
-    categories = Category.objects.all()
-    for category in categories:
-        subscribers = category.subscribers.all()
-        posts = Post.objects.filter(category=category, time_date__gte=datetime.now() - datetime.timedelta(days=7))
-        if posts.exists():
-            subject = f'Новые статьи в категории {category.get_category_name_display()}'
-            html_context = render_to_string(
-                'weekly_notifications_email.html',
-                {
-                    'category': category,
-                    'posts': posts,
-                }
-            )
-            msg = EmailMultiAlternatives(
-                subject=subject,
-                body='',
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                to=[s.email for s in subscribers],
-            )
-            msg.attach_alternative(html_context, 'text/html')
-            msg.send()
-
 
 @receiver(m2m_changed, sender=PostCategory)
 def notify_about_new_post(sender, instance, **kwargs):
