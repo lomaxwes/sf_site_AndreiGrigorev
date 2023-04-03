467b38ea57df7b26df9ca5f1cceac3286ced7f9c
lomaxwes <lomaxwes@gmail.com>
2023-04-03 14:45:52 +0500

    install redis and celery test

--- Project/NewsPaper/NewsPaper/celery.py
+++ Project/NewsPaper/NewsPaper/celery.py
@@ -8,11 +8,3 @@ app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
-
-
-app.conf.beat_schedule = {
-    'send_email_every_week': {
-        'task': 'news.tasks.send_weekly_email',
-        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
-    }
-}
