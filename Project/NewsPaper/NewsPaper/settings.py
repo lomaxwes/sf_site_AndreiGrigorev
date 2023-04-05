15d9d6ae2184c3b49d948f06ea717c4f7d009c36
lomaxwes <lomaxwes@gmail.com>
2023-04-05 15:20:34 +0500

    caches

--- Project/NewsPaper/NewsPaper/settings.py
+++ Project/NewsPaper/NewsPaper/settings.py
@@ -176,3 +176,10 @@ CELERY_RESULT_BACKEND = RED_URL
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache_files'), # Указываем, куда будем сохранять кэшируемые файлы! Не забываем создать папку cache_files внутри папки с manage.py!
+    }
+}
--- Project/NewsPaper/posts/urls.py
+++ Project/NewsPaper/posts/urls.py
@@ -4,17 +4,19 @@ from django.urls import path
 from .views import PostList, NewsList, NewsUpdate, PostDetail, ArticlesList, NewsDelete, CategoriesList, \
    PostsInCategory, HelloView
 from .views import PostSearch, NewsCreate, ArticlesCreate, ArticlesUpdate, ArticlesDelete, subscribe,  unsubscribe, CategoryDetail
+from django.views.decorators.cache import cache_page
+
 
 urlpatterns = [
-   path('', PostList.as_view(), name='post_list'),
+   path('', cache_page(60)(PostList.as_view()), name='post_list'),
    path('hello', HelloView.as_view(), name='hello'),
    path('search/', PostSearch.as_view(), name='post_search'),
    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
-   path('news/', NewsList.as_view(), name='news_list'),
+   path('news/', cache_page(300)(NewsList.as_view()), name='news_list'),
    path('news/create', NewsCreate.as_view(), name='news_create'),
    path('news/<int:pk>/update', NewsUpdate.as_view(), name='news_update'),
    path('news/<int:pk>/delete', NewsDelete.as_view(), name='post_delete'),
-   path('articles/', ArticlesList.as_view(), name='articles_list'),
+   path('articles/', cache_page(ArticlesList.as_view()), name='articles_list'),
    path('articles/create', ArticlesCreate.as_view(), name='articles_create'),
    path('articles/<int:pk>/update', ArticlesUpdate.as_view(), name='articles_update'),
    path('articles/<int:pk>/delete', ArticlesDelete.as_view(), name='post_delete'),
